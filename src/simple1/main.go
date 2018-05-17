ee12317accca974a22f9ffba9f95cd84cb30172f
Edward Tsang <etsang@splunk.com>
2018-05-17 13:18:40 -0700

    init logger from json payload

--- src/simple1/main.go
+++ src/simple1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+    "encoding/json"
 
 	"go.uber.org/zap"
 )
@@ -56,4 +57,49 @@ func main() {
 	slogger.Info("Info() uses sprint")
 	slogger.Infof("Infof() uses %s", "sprintf")
 	slogger.Infow("Infow() allows tags", "name", "Legolas", "type", 1)
+
+    fmt.Printf("*** Build a logger from a json")
+    rawJSONConfig := []byte(`{
+      "level": "debug",
+      "encoding": "console",
+      "outputPaths": ["stdout", "/tmp/logs"],
+      "errorOutputPaths": ["stderr"],
+      "initialFields": {"foo": "bar"},
+      "encoderConfig": {
+        "messageKey": "message",
+        "levelKey": "level",
+        "nameKey": "logger",
+        "timeKey": "time",  
+        "callerKey": "logger",
+        "stacktraceKey": "stacktrace",          
+        "callstackKey": "callstack",
+        "errorKey": "error",
+        "fileKey": "file",
+        "levelEncoder": "capitalColor",
+        "timeEncoder": "iso8601",
+        "durationEncoder": "nanos",
+        "callerEncoder": "full",
+        "nameEncoder": "full",
+        "sampling": {
+            "initial": "5",
+            "thereafter": "10"
+        }
+      }
+    }`)
+    config := zap.Config{}
+    if err := json.Unmarshal(rawJSONConfig, &config); err != nil {
+        panic(err)
+    }
+    logger, err := config.Build()
+    if err != nil {
+        panic(err)
+    }
+    logger.Debug("This is a DEBUG message")
+    logger.Info("This should have an ISO8601 based time stamp")
+    logger.Warn("This is a WARN message")
+    logger.Error("This is an ERROR message")
+    // logger.Fatal("This is a FATAL message")   // would exit if uncommented
+    // logger.DPanic("This is a DPANIC message") // would exit if uncommented
+
+
 }
